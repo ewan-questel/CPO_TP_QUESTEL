1b500aa15579c345d3119bd08c321b3fda553bb3
ewan questel <ew.questel@gmail.com>
2023-10-20 15:43:07 +0200

    Copie de SuperPuissance4 pour mode graphique

---
+++ SuperPuissance4_modeGraphique/src/superpuissance4_console/SuperPuissance4_console.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package superpuissance4_console;
+
+/**
+ *
+ * @author bdarties
+ */
+public class SuperPuissance4_console {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+
+        System.out.println("Vous entrez dans le jeux puissance 4 ! :\uD83D\uDE03");
+        Partie une_partie = new Partie();
+        une_partie.debuterPartie();
+        
+    }
+    
+}
